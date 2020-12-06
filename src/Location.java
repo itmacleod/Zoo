fecc52f3285c00dee0520dd87559565ee4e2d72f
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-12-06 14:38:03 -0500

    Added comments to classes

--- src/Location.java
+++ src/Location.java
@@ -2,17 +2,30 @@ import java.util.LinkedList;
 
 public class Location {
 	
+	//instance variables
 	private String name;
 	private LinkedList<String> animals = new LinkedList<>();
 	
+	/**
+	 * non-default constructor that initializes the name of the newly instantiated location
+	 * @param name name of the location
+	 */
 	public Location(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Setter method that sets the name of the current location
+	 * @param name name to be set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * getter method that returns the name of the location
+	 * @return name of the location
+	 */
 	public String getLocationName() {
 		return this.name;
 	}
--- src/Visitor.java
+++ src/Visitor.java
@@ -1,12 +1,22 @@
 
 public class Visitor {
 	
+	//instance variables
+	
 	private Location currentLocation;
 	
+	/**
+	 * getter method that returns current location
+	 * @return the visitor's current location
+	 */
 	public Location getLocation() {
 		return this.currentLocation;
 	}
 	
+	/**
+	 * setter method that sets the visitors current location
+	 * @param l current location to be set
+	 */
 	public void setCurrentLocation(Location l) {
 		this.currentLocation = l;
 	}
