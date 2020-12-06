86f598bcd33fc43360631487132ccd2a29b663aa
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-12-06 14:48:31 -0500

    Began working on big cat code

--- src/Location.java
+++ src/Location.java
@@ -7,11 +7,26 @@ public class Location {
 	private LinkedList<String> animals = new LinkedList<>();
 	
 	/**
-	 * non-default constructor that initializes the name of the newly instantiated location
+	 * non-default constructor that only takes a name
 	 * @param name name of the location
 	 */
 	public Location(String name) {
 		this.name = name;
+	}
+	
+	/**
+	 * non-default constructor that initializes the name of the newly instantiated location, as well as adds the animals in that exhibit to the animals linkedlist
+	 * @param name name of the location
+	 * @param animal1 name of animal1
+	 * @param animal2 name of animal2
+	 * @param animal3 name of animal3
+	 */
+	public Location(String name, String animal1, String animal2, String animal3) {
+		this.name = name;
+		this.animals.add(animal1);
+		this.animals.add(animal2);
+		this.animals.add(animal3);
+		
 	}
 	
 	/**
@@ -28,6 +43,14 @@ public class Location {
 	 */
 	public String getLocationName() {
 		return this.name;
+	}
+	
+	/**
+	 * getter method that returns the animals linkedlist
+	 * @return animals linkedlist
+	 */
+	public LinkedList<String> getAnimals(){
+		return this.animals;
 	}
 
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -14,11 +14,17 @@ public class Zoo {
 			System.out.println("Welcome to the zoo!");
 			System.out.println("You are currently at the entrance...Where would you like to go? (Cats/Birds/Lizards) ");
 			String goToLocation = stdin.next();
-			if(goToLocation.equals("Cats")) {
+			//visiting the cats
+			if(goToLocation.equalsIgnoreCase("Cats")) {
 				System.out.println("Great! Lets go see the tiger!");
-				Location location2 = new Location("Big Cat Den");
+				Location location2 = new Location("Big Cat Den", "tiger", "lion", "cheetah");
 				visitor.setCurrentLocation(location2);
-				System.out.println(visitor.getLocation().getLocationName());
+				System.out.println("The " + location2.getLocationName() + " exhibit has the following animals");
+				for(String animal : location2.getAnimals()) {
+					System.out.println("-" + animal);
+				}
+				
+				
 				visitZoo = false;
 			}
 		}
